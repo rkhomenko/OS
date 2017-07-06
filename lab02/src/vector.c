17653489cab04ca5a3271afa4370a003e5ee30c2
rkhomenko <romankhomenko1995@gmail.com>
2017-07-06 22:23:24 +0300

    Fix include's

--- lab02/src/vector.c
+++ lab02/src/vector.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <vector.h>
-#include <stdio.h>
 
 static vec_t* vec_resize(vec_t* vec, size_t new_size) {
     vec->data = realloc(vec->data, vec->elem_size * new_size);
